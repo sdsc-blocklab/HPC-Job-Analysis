f5d0344f80bbcc5da25e4636cccc7fbfbdb73ea0
kmiyachi <kmiyachi@ucsd.edu>
2019-07-15 18:31:05 -0700

    small fixes

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require("Migrations");
 const BlockLab = artifacts.require("BlockLab");
+const Job = artifacts.require("Job");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(BlockLab);
+  deployer.deploy(Job);
 };
